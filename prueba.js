9fb1e1e35f881394a8ad40a059b374bf4fcd4a50
jean urena <jeanpaul_34_1@hotmail.com>
2021-03-18 15:32:41 -0400

    sumarnumeros

--- prueba.js
+++ prueba.js
@@ -1,2 +1,24 @@
 //Esta es una linea de Test
 //Esta es otra linea agregada
+
+
+let valo1 = 20;
+let valor2 = 30;
+
+let objeto_general = {
+    suma: function(a,b){
+return a + b
+    }, 
+    resta: function(a,b){
+        return a -b;
+    },
+    multiplicacion: function(a,b){
+        return a * b;
+    },
+    division: function (a,b){
+        return a / b;
+    }
+}
+
+
+objeto_general.suma(20,30);
